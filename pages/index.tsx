74d3c6d2466be3fb6628103043e11ebf7356da52
Bradley Windybank <bradley.windybank@trademe.co.nz>
2024-03-10 15:05:11 +1300

    Add vercel analytics

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,14 @@ import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import SocialLinks from "../components/social-links";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Home() {
   return (
     <Layout home>
       <Head>
         <SpeedInsights />
+        <Analytics />
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMdCentre}>
